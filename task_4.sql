dcfe00fa7db7e445fb74f96a10159ed7c8580066
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-12-12 06:04:24 +0300

    wrote code for explanation of Books table

---
+++ task_4.sql
@@ -0,0 +1,11 @@
+-- Print full description of the BOOKS table
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+  AND TABLE_NAME = 'BOOKS';
